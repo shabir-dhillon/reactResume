98ca862ed2bca2d11ac0f735bb8d76f45994f96e
shabir-dhillon <59631293+shabir-dhillon@users.noreply.github.com>
2021-04-17 01:14:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
 import Contact from './components/contactInfo';
 import Educations from './components/education';
 import Experience from './components/experience';
